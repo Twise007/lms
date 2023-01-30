7ce50e70072f76e71884f89d5772256f81467d3b
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-01-30 13:04:05 +0100

    login home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Homepic from '../assets/homePic.jpg'
+import { BiUserCircle } from 'react-icons/bi'
 
 
 const Home = () => {
@@ -11,21 +12,27 @@ const Home = () => {
         <div className="hero min-h-screen text-bg-off-white" style={{backgroundImage: `url(${Homepic})`}}>
         <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content flex-col lg:flex-row">
-            <div className='p-5'>
+            <div className=''>
               <h1 className="text-5xl font-bold">Learning Management System</h1>
               <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
             </div>
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-bg-white">
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl glass">
               <div className="card-body">
+                <div className="card-actions justify-center">
+                  <div className="text-bg-off-white">
+                    <BiUserCircle size={45} className="text-bg-white"/>
+                    <hi style={{fontSize:"20px"}} className='items-center'>Login</hi>
+                  </div>
+                </div>
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text text-bg-purple">Email</span>
+                    <span className="label-text text-bg-white">Email</span>
                   </label>
                   <input type="text" placeholder="email" className="input input-bordered bg-bg-off-white text-bg-black" />
                 </div>
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text text-bg-purple">Password</span>
+                    <span className="label-text text-bg-white">Password</span>
                   </label>
                   <input type="text" placeholder="password" className="input input-bordered bg-bg-off-white text-bg-black" />
                   <label className="label">
