c29bb03b57064976b1c1ab8f09b9cc05f7764330
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-03-03 02:44:15 +0100

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ const Search = ({value, onChange}) => {
       <div className="form-control text-bg-black rounded-box bg-bg-white">
           <div className="input-group">
               <input type="text" placeholder="Search Staff" value={value} onChange={onChange} className="placeholder:italic input input-bordered bg-bg-white text-bg-black" style={{width:'100%'}}/>
-              <span className="bg-bg-purple text-bg-white bold">
+              <span className="btn bg-bg-purple text-bg-white hover:bg-bg-white hover:text-bg-purple bold" style={{border:"none"}}>
                   <BiSearch size={20}/>
               </span>
           </div>
