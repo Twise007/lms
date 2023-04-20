1f856e6e6dd1d72c7f5250cb1a11d02856580e90
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-04-20 06:57:11 +0100

    notice

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
-import { FaUserCircle } from 'react-icons/fa'
-import { RxDropdownMenu } from 'react-icons/rx'
-import { Link } from 'react-router-dom'
-import Htitle from './Htitle'
-
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { RxDropdownMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
+import Htitle from "./Htitle";
 
 const Header = () => {
-
   return (
-    <div className="navbar bg-bg-white"style={{position:"fixed", zIndex:"12"}}>
-        <Htitle/>
-        <div className="flex-none text-bg-black">
-            <ul className="menu menu-horizontal px-1">
-                <li><p><FaUserCircle size={20} />Hi,<span className='text-bg-purple'>Tunde</span></p></li>
-            </ul>
-            <div className="dropdown dropdown-end">
-                <li tabIndex={0} className={`btn btn-ghost btn-circle`}>
-                    <p><RxDropdownMenu size={25}/></p>
-                </li>
-                <ul tabIndex={0} className={`menu menu-compact dropdown-content mt-3 p-2 shadow bg-bg-white rounded-box w-52 duration-700`}> 
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/">Logout</Link></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <div
+      className="navbar bg-bg-white"
+      style={{ position: "fixed", zIndex: "12" }}
+    >
+      <Htitle />
+      <h1 className="mb-3 text-3xl font-bold text-[red]">UNDER CONSTRUCTION</h1>
+      <div className="flex-none text-bg-black">
+        <ul className="px-1 menu menu-horizontal">
+          <li>
+            <p>
+              <FaUserCircle size={20} />
+              Hi,<span className="text-bg-purple">Tunde</span>
+            </p>
+          </li>
+        </ul>
 
-export default Header
+        <div className="dropdown dropdown-end">
+          <li tabIndex={0} className={`btn btn-ghost btn-circle`}>
+            <p>
+              <RxDropdownMenu size={25} />
+            </p>
+          </li>
+          <ul
+            tabIndex={0}
+            className={`menu menu-compact dropdown-content mt-3 p-2 shadow bg-bg-white rounded-box w-52 duration-700`}
+          >
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/">Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,9 +11,6 @@ const Home = () => {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="text-center hero-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold text-[red]">
-              UNDER CONSTRUCTION
-            </h1>
             <h1 className="mb-5 text-5xl font-bold">
               Learning Management System
             </h1>
